7dd01f6129391f36aaf77f2a5f74d877e602f460
Matthew Berger <bergermaestro@gmail.com>
2022-03-15 10:58:27 -0400

    Fixed Nav Overflow

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,10 +6,12 @@ const Nav = () => {
     return (
             <nav className="py-6 bg-gradient-to-b from-default-900">
                 <div className="px-6 md:px-14 lg:px-6 lg:max-w-5xl mx-auto flex flex-col sm:flex-row justify-between items-center">   
+                    <span className="pb-3 sm:py-0 cursor-pointer">
                     <Link href="/">
                         <Image src={NavImage}/>
                     </Link>
-                    <div className='flex flex-row space-x-12 uppercase text-lg items-center'>
+                    </span>
+                    <div className='flex flex-row sm:space-x-12 uppercase text-lg items-center justify-between w-full sm:w-auto sm:justify-center'>
                         <Link href="/"><a className='hover:text-brandTeal transition-all'>Home</a></Link>
                         <Link href="/about"><a className='hover:text-brandTeal transition-all'>About</a></Link>
                         <Link href="/projects"><a className='hover:text-brandTeal transition-all'>Projects</a></Link>
