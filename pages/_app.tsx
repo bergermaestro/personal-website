3b349f10b5f01367421038872013ed7aac335186
Matthew Berger <bergermaestro@gmail.com>
2022-03-21 01:38:36 -0400

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,39 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
+
+import * as ga from '../lib/google-analytics'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (<>
+  const router = useRouter()
 
-    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+  useEffect(() => {
+    const handleRouteChange = (url:any) => {
+      ga.pageview(url)
+    }
 
-    <Script strategy="lazyOnload">
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return(
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy='afterInteractive' />
+      <Script id="google-analytics" strategy='afterInteractive'>
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-            });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
-    </Script>
-    
-    <Component {...pageProps} />
-  </>
-  );
+      </Script>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
